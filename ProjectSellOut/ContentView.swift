a13232123cc3225dc4687505422944d947e333ee
Thomas <tommyprezioso@gmail.com>
2020-12-15 20:57:18 -0500

    Initial Commit

---
+++ ProjectSellOut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectSellOut
+//
+//  Created by Thomas Prezioso Jr on 12/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectSellOut/ProjectSellOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSellOutApp.swift
+//  ProjectSellOut
+//
+//  Created by Thomas Prezioso Jr on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSellOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
