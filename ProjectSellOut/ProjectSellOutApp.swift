37c88bc4937d1a243d2d8ab0aeeaece772a77e02
Thomas <tommyprezioso@gmail.com>
2020-12-16 19:11:02 -0500

    added core data

--- ProjectSellOut/ProjectSellOutApp.swift
+++ ProjectSellOut/ProjectSellOutApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
+
 struct ProjectSellOutApp: App {
+    let persistence = PersistenceManager()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class PersistenceManager {
+  let persistentContainer: NSPersistentContainer = {
+      let container = NSPersistentContainer(name: "ProjectSellOut")
+      container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+          if let error = error as NSError? {
+              fatalError("Unresolved error \(error), \(error.userInfo)")
+          }
+      })
+      return container
+  }()
 }
