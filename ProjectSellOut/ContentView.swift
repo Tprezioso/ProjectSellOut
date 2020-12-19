45e26c5cf49e1711f09be8ce44f9cce4613f5ee3
Thomas <tommyprezioso@gmail.com>
2020-12-18 19:46:44 -0500

    hooked up coredata to main content view

--- ProjectSellOut/ContentView.swift
+++ ProjectSellOut/ContentView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var taskName: String = ""
+    @Environment(\.managedObjectContext) var context
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+                TextField("Task Name", text: $taskName)
+                Button(action: {
+                    self.addTask()
+                }){
+                    Image(systemName: "plus.circle.fill")
+                }
+            }            .padding()
+    }
+
+    func addTask() {
+        let newTask = Task(context: context)
+        newTask.id = UUID()
+        newTask.isCompleted = false
+        newTask.name = taskName
+        newTask.dateAdded = Date()
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ ProjectSellOut/Task+Extensions.swift
@@ -0,0 +1,11 @@
+//
+//  Task+Extensions.swift
+//  ProjectSellOut
+//
+//  Created by Thomas Prezioso Jr on 12/18/20.
+//
+
+import Foundation
+
+//extension Task: Identifiable {
+//}
